3035f5ce72e11cca157ed5dfe651f1569cd0b65e
Leonardo Neri <leonardoneri01@outlook.com>
2022-05-21 04:31:07 -0300

    Inicio Teste Ideal Trends

---
+++ js/funcoes.js
@@ -0,0 +1,28 @@
+$(window).load(function() {
+    $('#slider').nivoSlider({
+        effect: 'random',
+        slices: 15,
+        boxCols: 8,
+        boxRows: 4,
+        animSpeed: 500,
+        pauseTime: 3000,
+        startSlide: 0,
+        directionNav: true,
+        controlNav: true,
+        controlNavThumbs: false,
+        pauseOnHover: true,
+        manualAdvance: false,
+        prevText: '<i class="fa-solid fa-circle-arrow-left"></i>',
+        nextText: '<i class="fa-solid fa-circle-arrow-right"></i>',
+        randomStart: false,
+        beforeChange: function(){},
+        afterChange: function(){},
+        slideshowEnd: function(){},
+        lastSlide: function(){},
+        afterLoad: function(){}
+    });
+});
+
+$(window).load(function() {
+    $('#slider').nivoSlider();
+});
